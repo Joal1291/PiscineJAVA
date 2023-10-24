972dcd03bc389a5ab6eef1a51e24654b3b50dc86
Joal1291 <jordan.albert@github.com>
2023-10-24 11:28:45 +0200

    ex 4 complete

---
+++ day2/ex_04/Test.java
@@ -0,0 +1,21 @@
+public class Test{
+    public static void main(String[] args){
+        Gecko arthur = new Gecko("Arthur", 32);
+
+        arthur.setAge(1);
+        arthur.status();
+
+        arthur.setAge(2);
+        arthur.status();
+
+        arthur.setAge(4);
+        arthur.status();
+
+        arthur.setAge(11);
+        arthur.status();
+
+        arthur.setAge(56);
+        arthur.status();
+
+    }   
+}
