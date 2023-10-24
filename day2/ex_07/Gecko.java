33b10e60f12f717562b9b9e6c07b50a089af2e9a
Joal1291 <jordan.albert@github.com>
2023-10-24 14:17:08 +0200

    ex 7 complete

---
+++ day2/ex_07/Gecko.java
@@ -0,0 +1,106 @@
+
+public class Gecko {
+    private String name;
+    private int age;
+    private int energy = 25;
+
+    Gecko(){
+        this.name = "Unknown";
+        System.out.println("Hello!");
+    }
+    
+    Gecko(String name, int age){
+        this.name = name;
+        this.age = age;
+
+        System.out.println("Hello " + name + "!");
+    }
+
+    // --------Getter, Setter
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public int getEnergy(){
+        return energy;
+    }
+    public void setEnergy(int energy){
+        if(energy > 100){
+            this.energy = 100;
+        }else if(energy < 0){
+            this.energy = 0;
+        }else{
+            this.energy = energy;
+        }
+    }
+
+    // --------Method
+    public void status (){
+        switch(this.age){
+            case 0:
+                System.out.println("Unborn Gecko");
+                break;
+            case 1:
+            case 2:
+                System.out.println("Baby Gecko");
+                break;
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+                System.out.println("Adult Gecko");
+                break;
+            case 11:
+            case 12:
+            case 13:
+                System.out.println("Old Gecko");
+                break;
+            default:
+                System.out.println("Impossible Gecko");
+        }
+    }
+
+    public void hello(String params){
+        System.out.println("Hello " + params +", I'm " + this.name);
+    }
+    public void hello(int params){
+        for(int i = 1; i <= params; i++){
+            System.out.println("Hello, I'm " + this.name);
+        }
+    }
+
+    public void eat(String params){
+        String newparams = params.toLowerCase();
+        switch (newparams){
+            case "meat":
+                System.out.println("Yummy!");
+                setEnergy(this.energy + 10);
+                break;
+            case "vegetable":
+                System.out.println("Erk!");
+                setEnergy(this.energy - 10);
+                break;
+            default:
+                System.out.println("I can't eat this!");
+        }
+    }
+
+    public void work(){
+        if(this.energy >= 25){
+            setEnergy(this.energy - 9 );
+            System.out.println("Im working T.T");
+        } else if(this.energy <= 24){
+            setEnergy(this.energy +50);
+            System.out.println("Heyyy I'm to sleepy, better take a nap!");
+        }
+    }
+}
---
+++ day2/ex_07/Test.java
@@ -0,0 +1,8 @@
+public class Test{
+    public static void main(String[] args){
+        Gecko arthur = new Gecko("Arthur", 32);
+
+            arthur.work();
+            System.out.println(arthur.getEnergy());
+    }   
+}
