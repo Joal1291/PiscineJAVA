0e9e4979c16bd80f0cb1d2f0c008663a2d33b31d
Joal1291 <jordan.albert@github.com>
2023-10-29 12:41:38 +0100

    ex 7 complete

--- day5/untitled/src/main/java/Main/RadScorpion.java
+++ day5/untitled/src/main/java/Main/RadScorpion.java
@@ -1,10 +1,34 @@
 package Main;
-
+import java.util.Objects;
 public class RadScorpion extends Monster{
 
     public RadScorpion(String name){
         super(name, 80, 50, 25, 8);
         System.out.println(this.getName() + ": Grrr!");
 
+    }
+
+    @Override
+    public boolean attack(Fighter fighter) {
+        if(fighter instanceof AssaultTerminator){
+            return super.attack(fighter);
+        }else{
+            if(!Objects.equals(fighter.getId(), getTarget().getId())){
+                System.out.println(this.name + ": I'm to far from " + fighter.getName());
+                return false;
+            }else if(Objects.equals(fighter.getId(), this.id)){
+                return false;
+            }else{
+                if(apcost <= this.ap){
+                    this.ap = this.ap - this.apcost;
+                    System.out.println(this.name + " attacks " + getTarget().getName());
+                    int doubleDamage = this.damage * 2;
+                    getTarget().receiveDamage(doubleDamage);
+                    return true;
+                }else{
+                    return false;
+                }
+            }
+        }
     }
 }
--- day5/untitled/src/main/java/Main/SuperMutant.java
+++ day5/untitled/src/main/java/Main/SuperMutant.java
@@ -2,9 +2,20 @@ package Main;
 
 public class SuperMutant extends Monster{
 
+    // --- Variable
+
+    // --- Constructor
     public SuperMutant(String name){
         super(name, 170, 20, 60, 20);
-        System.out.println(this.getName() + ": Roaarrr");
+        System.out.println(this.name + ": Roaarrr");
 
+    }
+    // --- Getter followed by Setter.
+
+    //  --- Method
+    @Override
+    public void recoverAP() {
+        super.recoverAP();
+        this.hp = this.hp + 10;
     }
 }
